9af716739f50decf89a6af94bb01e74f4ef1b1d8
Mykhailo <sharkon90@gmail.com>
2024-02-07 18:55:44 -0500

    adding queries and mutations to react saved book screen

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,12 +1,18 @@
 import {useState, useEffect} from "react";
 import {Container, Card, Button, Row, Col} from "react-bootstrap";
 
-import {useMutation} from "@apollo/client";
-import {getMe, deleteBook} from "../utils/API";
+import {useMutation, useQuery} from "@apollo/client";
+import {DELETE_BOOK} from "../utils/mutations";
+import {QUERY_USER} from "../utils/queriess";
+// import {getMe, deleteBook} from "../utils/API";
 import Auth from "../utils/auth";
 import {removeBookId} from "../utils/localStorage";
 
 const SavedBooks = () => {
+  const [loading, data] = useQuery(QUERY_USER);
+  const [deleteBook, {error}] = useMutation(DELETE_BOOK, {
+    refetchQueries: [QUERY_USER, "user"],
+  });
   const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
